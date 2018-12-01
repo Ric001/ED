70334f783744e059d9f314c1366ca76fe9309cbe
= <josericardo29@hotmail.es>
2018-12-01 15:40:56 -0400

    Listas las funcionalidades para append y preppend de la lista.

--- LinkedLists/List.h
+++ LinkedLists/List.h
@@ -3,7 +3,7 @@
 
 #include <iostream>
 using std::cout;
-
+using std::endl;
 #include "Node.h"
 
 template< typename TIPONODO >
@@ -26,7 +26,57 @@ class List {
 
 template< typename TIPONODO >
 List <TIPONODO>::List()
-:{
+: first( NULL ), last( NULL ) {
+}
 
+template< typename TIPONODO >
+List <TIPONODO>::~List() {
+    if ( !isEmpty() ) {
+        cout << "Destruyendo nodes...\n";
+        
+        Nodo < TIPONODO > *actual = first;
+        Nodo < TIPONODO > *temporal = NULL;
+
+        while ( actual != NULL ) {
+            temporal = actual;
+            cout << "Borrando: " << temp->dato << '\n';
+            actual = actual->_siguiente;
+            delete temporal;
+        }
+        cout << "Todos los nodos han sido destruidos" << endl;
+    }
+
+    cout << "Advertencia: Lista Vacia al momento de su destruccion" << endl;
+}
+
+
+template <typename TIPONODO >
+void List <TIPONODO>::append( const TIPONODO & value)
+{
+    Nodo<TIPONODO> nuevoNodo = getNewNode( value );
+    if ( isEmpty() ) {
+        first = nuevoNodo;
+        last = nuevoNodo;
+    } 
+    else
+    {
+        last->siguiente = nuevoNodo;
+        last = nuevoNodo; 
+    }
+}
+
+template<typename TIPONODO>
+void List<TIPONODO>::preppend( const TIPONODO & value ) {
+    NodoLista < TIPONODO > *nuevoNodo = getNewNode( nodo );
+
+    if ( isEmpty() ) {
+        first = nuevoNodo;
+        last = nuevoNodo;
+    } 
+    else 
+    {
+        nuevoNodo->siguiente = first;
+        first = nuevoNodo;
+    }
 }
 #endif
